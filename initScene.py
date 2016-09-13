5261315912ad8db37b42ae4f7b3d234a4f3828db
Fabian Fritz <fabianfuture@web.de>
2016-09-13 18:06:54 +0200

    Dynamically add cameras

--- initScene.py
+++ initScene.py
@@ -1,8 +1,32 @@
 import sys
 import bpy
+import numpy as np
+
+scene = bpy.context.scene
+
+scene.unit_settings.system = bpy.utils.units.systems.METRIC
+#scene.unit_settings.scale_length = 0.001
+
+for a in bpy.context.screen.areas:
+	if a.type == 'VIEW_3D':
+		for s in a.spaces:
+			if s.type == 'VIEW_3D':
+				s.clip_end = 10000
 
 origin=(0,0,0)
-bpy.ops.mesh.primitive_uv_sphere_add(location=origin)
+bpy.ops.mesh.primitive_ico_sphere_add(location=origin, size=35, subdivisions=4)
 
-bpy.data.scenes['Scene'].render.filepath = '/home/fabian/code/scanner-simulation/image.jpg'
-bpy.ops.render.render( write_still=True )
+for cameraName, offset in zip(['05_A','05_B'],[100.0,-100.0]):
+	cam = bpy.data.cameras.new("Camera")
+	cameraObj = bpy.data.objects.new(cameraName, cam)
+	scene.objects.link(cameraObj)
+
+	cameraObj.location = (offset,-1000.0,0.0)
+	cameraObj.rotation_euler = (np.pi/2,0.0,0.0)
+	cameraObj.scale = (1000,1000,1000)
+	cam.clip_end = 10000
+
+	scene.camera = cameraObj
+
+	bpy.data.scenes['Scene'].render.filepath = '/home/fabian/code/scanner-simulation/image%s.jpg' % cameraName
+	bpy.ops.render.render( write_still=True )
--- simulator.py
+++ simulator.py
@@ -1,7 +1,26 @@
 #! /usr/bin/python
 
+import sys
 import subprocess
 import shlex
+import numpy as np
+
+def readTKA(fileName):
+	tkaFile = open(fileName, 'r')
+	lines = tkaFile.readlines()
+
+	rotationLines = lines[1:4]
+	rotationMatrix = list()
+	for line in rotationLines:
+		line = line.split()
+		line = [float(i) for i in line]
+		rotationMatrix.append(line)
+	rotationMatrix = np.asarray(rotationMatrix)
+	X,Y,Z = [float(line.split()[1]) for line in lines[4:7]]
+	f,K1,K2,S,x,y,a,b = [float(line.split()[1]) for line in lines[7:15]]
+	rs = [float(i) for i in lines[15].split()[1:3]]
+	c = float(lines[16].split()[1])
+	return rotationMatrix,X,Y,Z,f,K1,K2,S,x,y,a,b,rs,c
 
 if __name__ == '__main__':
 	cmd = 'blender empty.blend --background --python initScene.py -- test'
