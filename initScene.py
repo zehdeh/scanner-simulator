a0d5457809a073b1db8115c6eb8cb5257fd2be98
Fabian Fritz <fabianfuture@web.de>
2016-09-20 18:00:24 +0200

    Implement camera translation

--- initScene.py
+++ initScene.py
@@ -1,6 +1,25 @@
+import os
 import sys
 import bpy
 import numpy as np
+
+def readTKA(fileName):
+	tkaFile = open(fileName, 'r')
+	lines = tkaFile.readlines()
+
+	rotationLines = lines[1:4]
+	rotationMatrix = list()
+	for line in rotationLines:
+		line = line.split()
+		line = [float(i) for i in line]
+		rotationMatrix.append(line)
+	rotationMatrix = np.asarray(rotationMatrix)
+	X,Y,Z = [float(line.split()[1]) for line in lines[4:7]]
+	f,K1,K2,S,x,y,a,b = [float(line.split()[1]) for line in lines[7:15]]
+	rs = [float(i) for i in lines[15].split()[1:3]]
+	c = float(lines[16].split()[1])
+	return rotationMatrix,X,Y,Z,f,K1,K2,S,x,y,a,b,rs,c
+
 
 scene = bpy.context.scene
 
@@ -16,17 +35,32 @@ for a in bpy.context.screen.areas:
 origin=(0,0,0)
 bpy.ops.mesh.primitive_ico_sphere_add(location=origin, size=35, subdivisions=4)
 
-for cameraName, offset in zip(['05_A','05_B'],[100.0,-100.0]):
+calibrationDir = 'Calibrations/20160913110710522/'
+for tkaFile in [f for f in os.listdir(calibrationDir) if f.endswith('.tka')]:
+	calibProperties = readTKA(calibrationDir + tkaFile)
+	R = calibProperties[0]
+	T = calibProperties[1:4]
+	R2 = np.eye(4)
+	R2[:3,:3] = R
+	R2[:3,3] = T
+	np.set_printoptions(suppress=True)
+	print(tkaFile[:-4])
+	R3 = np.asarray([np.arctan2(-R[1,2],R[1,1]), np.arctan2(-R[2,0],R[0,0]), np.arcsin(R[1,0])])
+
 	cam = bpy.data.cameras.new("Camera")
-	cameraObj = bpy.data.objects.new(cameraName, cam)
+	cameraObj = bpy.data.objects.new(tkaFile[:-4], cam)
 	scene.objects.link(cameraObj)
 
-	cameraObj.location = (offset,-1000.0,0.0)
-	cameraObj.rotation_euler = (np.pi/2,0.0,0.0)
+	pos = R2.dot([0,0,0,1])
+	#R3 = R2.dot([0,0,-1,0])
+	print(R3)
+
+	cameraObj.location = (pos[0],pos[2],pos[1])
+	cameraObj.rotation_euler = (R3[0],np.pi - R3[1],R3[2])
 	cameraObj.scale = (1000,1000,1000)
 	cam.clip_end = 10000
 
 	scene.camera = cameraObj
 
-	bpy.data.scenes['Scene'].render.filepath = '/home/fabian/code/scanner-simulation/image%s.jpg' % cameraName
-	bpy.ops.render.render( write_still=True )
+	#bpy.data.scenes['Scene'].render.filepath = '/home/fabian/code/scanner-simulation/image%s.jpg' % cameraName
+	#bpy.ops.render.render( write_still=True )
--- simulator.py
+++ simulator.py
@@ -4,24 +4,27 @@ import sys
 import subprocess
 import shlex
 import numpy as np
+import cv2
 
-def readTKA(fileName):
-	tkaFile = open(fileName, 'r')
-	lines = tkaFile.readlines()
-
-	rotationLines = lines[1:4]
-	rotationMatrix = list()
-	for line in rotationLines:
-		line = line.split()
-		line = [float(i) for i in line]
-		rotationMatrix.append(line)
-	rotationMatrix = np.asarray(rotationMatrix)
-	X,Y,Z = [float(line.split()[1]) for line in lines[4:7]]
-	f,K1,K2,S,x,y,a,b = [float(line.split()[1]) for line in lines[7:15]]
-	rs = [float(i) for i in lines[15].split()[1:3]]
-	c = float(lines[16].split()[1])
-	return rotationMatrix,X,Y,Z,f,K1,K2,S,x,y,a,b,rs,c
 
 if __name__ == '__main__':
-	cmd = 'blender empty.blend --background --python initScene.py -- test'
+	#calibProperties = readTKA(sys.argv[1])
+	#R = calibProperties[0]
+	#T = calibProperties[1:4]
+	#T = -R.dot(T)
+	#r,J = cv2.Rodrigues(np.require(R, dtype=np.float64))
+	#print np.asarray([np.arctan2(-R[1,2],R[1,1]), np.arctan2(-R[2,0],R[0,0]), np.arcsin(R[1,0])])*180/np.pi
+
+	"""
+	R2 = np.eye(4)
+	R2[:3,:3] = R
+	R2[:3,3] = T
+	np.set_printoptions(suppress=True)
+	print R2
+	print R2.dot([0,0,0,1])
+
+	sys.exit(0)
+	"""
+	#cmd = 'blender empty.blend --background --python initScene.py -- test'
+	cmd = 'blender empty.blend --python initScene.py -- test'
 	subprocess.check_call(shlex.split(cmd))
